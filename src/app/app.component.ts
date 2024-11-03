c096845dd68cde2a32a3a4f477a819213c9539a0
Milagros Ostos Norabuena <mili.ostos@gmail.com>
2024-11-03 18:59:03 -0500

    Fin de sección 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,24 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'bases';
+  //title = 'Mi primera app de Angular';
+  public title:string = 'Hola Mundo';
+  //public counter :number=10;
+
+  /*increaseBy():void{
+    this.counter+=1;
+  }
+  dicreaseBy():void{
+    this.counter-=1;
+  }*/
+
+  /*increaseBy(value:number):void{
+    this.counter+=value;
+  }
+  resetCounter(){
+    this.counter=10;
+  }*/
 }
